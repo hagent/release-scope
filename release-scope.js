fd98d77b0e24a9157ade1a687e77f4eec3bb244d
Alexey Tomin <alextomin@list.ru>
2022-04-01 17:09:48 +0200

    fix

--- release-scope.js
+++ release-scope.js
@@ -4,42 +4,44 @@ const fetch = require('isomorphic-fetch')
 const { execSync } = require('child_process');
 
 async function getReleaseHash() {
-  const releaseHash = await (await fetch('https://www.worldremit.com/public-assets/utils/cms_version.json')).json();
-  return releaseHash.commit.substr(0, 9);
+    const releaseHash = await (await fetch('https://www.worldremit.com/public-assets/utils/cms_version.json')).json();
+    return releaseHash.commit.substr(0, 9);
 }
 
 function getCommitHashes(branch, fromIndex = 0) {
-  return execSync(`git log origin/${branch} --format='%h'`, { encoding: 'utf-8' })
-    .split('\n')
-    .splice(fromIndex)
+    const branchName = branch ? `origin/${branch}` : branch;
+    return execSync(`git log origin/${branchName} --format='%h'`, { encoding: 'utf-8' })
+        .split('\n')
+        .splice(fromIndex)
 }
 
 function getCommitMessages(branch, fromIndex = 0) {
-  return execSync(`git log origin/${branch} --format='%s'`, { encoding: 'utf-8' })
-    .split('\n')
-    .splice(fromIndex)
+    const branchName = branch ? `origin/${branch}` : branch;
+    return execSync(`git log ${branchName} --format='%s'`, { encoding: 'utf-8' })
+        .split('\n')
+        .splice(fromIndex)
 }
 
 async function main() {
-  const [, , releaseBranch = '', masterBranch = 'master'] = process.argv;
-  // const res = execSync(`git log ${} --format='%h %s %d '`, { encoding: 'utf-8' });
-  execSync(`git fetch`)
-  const releaseBranchHistory = getCommitHashes(releaseBranch);
-  const releaseHash = await getReleaseHash();
-  // it's possible that release branch has unreleased changes, 
-  // after releaseCommitIndex index everythins is really released
-  const releaseCommitIndex = releaseBranchHistory.indexOf(releaseHash);
-  if (releaseCommitIndex === -1) {
-    throw new Error(`release hash ${releaseHash} wasnt found in release branch logs, first 30:
+    const [, , releaseBranch = '', masterBranch = 'master'] = process.argv;
+    // const res = execSync(`git log ${} --format='%h %s %d '`, { encoding: 'utf-8' });
+    execSync(`git fetch`)
+    const releaseBranchHistory = getCommitHashes(releaseBranch);
+    const releaseHash = await getReleaseHash();
+    // it's possible that release branch has unreleased changes, 
+    // after releaseCommitIndex index everythins is really released
+    const releaseCommitIndex = releaseBranchHistory.indexOf(releaseHash);
+    if (releaseCommitIndex === -1) {
+        throw new Error(`release hash ${releaseHash} wasnt found in release branch logs, first 30:
 ${releaseBranchHistory.slice(0, 30)}`)
-  }
-  console.log('releaseCommitIndex', releaseCommitIndex)
-  const releasedCommitMessagesSet = new Set(getCommitMessages(releaseBranch, releaseCommitIndex));
-  const masterCommitMessages = getCommitMessages(masterBranch)
-    .map((message, ind) => ({ message, ind })); // + "ind" field to save original index
-  const messagesNotInReleaseBranch = masterCommitMessages.filter(({message}) => !releasedCommitMessagesSet.has(message))
-  console.log('unreleased changes:')
-  console.log(messagesNotInReleaseBranch.map(x => x.message).join('\n'))
+    }
+    console.log('releaseCommitIndex', releaseCommitIndex)
+    const releasedCommitMessagesSet = new Set(getCommitMessages(releaseBranch, releaseCommitIndex));
+    const masterCommitMessages = getCommitMessages(masterBranch)
+        .map((message, ind) => ({ message, ind })); // + "ind" field to save original index
+    const messagesNotInReleaseBranch = masterCommitMessages.filter(({ message }) => !releasedCommitMessagesSet.has(message))
+    console.log('unreleased changes:')
+    console.log(messagesNotInReleaseBranch.map(x => x.message).join('\n'))
 }
 
 
