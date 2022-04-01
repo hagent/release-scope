7bb2bf1ce1b168af11a796510cf90178a4553514
Alexey Tomin <alextomin@list.ru>
2022-04-01 17:23:18 +0200

    fix getCurrentBranch

--- release-scope.js
+++ release-scope.js
@@ -23,12 +23,11 @@ function getCommitMessages(branch, fromIndex = 0) {
 }
 
 function getCurrentBranch() {
-    return execSync(`git rev-parse --abbrev-ref HEAD`, { encoding: 'utf-8' });
+    return execSync(`git rev-parse --abbrev-ref HEAD`, { encoding: 'utf-8' }).trim();
 }
 
 async function main() {
     const [, , releaseBranch = getCurrentBranch(), masterBranch = 'master'] = process.argv;
-    console.log({releaseBranch})
     // const res = execSync(`git log ${} --format='%h %s %d '`, { encoding: 'utf-8' });
     execSync(`git fetch`)
     const releaseBranchHistory = getCommitHashes(releaseBranch);
