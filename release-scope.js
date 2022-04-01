01fb5ea3144d78957b7d88239cb7c7ae399ec12a
Alexey Tomin <alextomin@list.ru>
2022-04-01 16:46:31 +0200

    init commit

---
+++ release-scope.js
@@ -0,0 +1,54 @@
+const fetch = require('isomorphic-fetch')
+const { execSync } = require('child_process');
+
+async function getReleaseHash() {
+  const releaseHash = await (await fetch('https://www.worldremit.com/public-assets/utils/cms_version.json')).json();
+  return releaseHash.commit.substr(0, 9);
+}
+
+function getCommitHashes(branch, fromIndex = 0) {
+  return execSync(`git log origin/${branch} --format='%h'`, { encoding: 'utf-8' })
+    .split('\n')
+    .splice(fromIndex)
+}
+
+function getCommitMessages(branch, fromIndex = 0) {
+  return execSync(`git log origin/${branch} --format='%s'`, { encoding: 'utf-8' })
+    .split('\n')
+    .splice(fromIndex)
+}
+
+async function main() {
+  const [, , releaseBranch = '', masterBranch = 'master'] = process.argv;
+  // const res = execSync(`git log ${} --format='%h %s %d '`, { encoding: 'utf-8' });
+  execSync(`git fetch`)
+  const releaseBranchHistory = getCommitHashes(releaseBranch);
+  const releaseHash = await getReleaseHash();
+  // it's possible that release branch has unreleased changes, 
+  // after releaseCommitIndex index everythins is really released
+  const releaseCommitIndex = releaseBranchHistory.indexOf(releaseHash);
+  if (releaseCommitIndex === -1) {
+    throw new Error(`release hash ${releaseHash} wasnt found in release branch logs, first 30:
+${releaseBranchHistory.slice(0, 30)}`)
+  }
+  console.log('releaseCommitIndex', releaseCommitIndex)
+  const releasedCommitMessagesSet = new Set(getCommitMessages(releaseBranch, releaseCommitIndex));
+  const masterCommitMessages = getCommitMessages(masterBranch)
+    .map((message, ind) => ({ message, ind })); // + "ind" field to save original index
+  const messagesNotInReleaseBranch = masterCommitMessages.filter(({message}) => !releasedCommitMessagesSet.has(message))
+  console.log('unreleased changes:')
+  console.log(messagesNotInReleaseBranch.map(x => x.message).join('\n'))
+}
+
+
+main()
+
+
+
+/*
+https://worldremit.atlassian.net/browse/CONWEB-1654: CMS Release 1.46 (#2989)
+https://worldremit.atlassian.net/browse/CONWEB-1744: Remove unnecessary aria-label (#2991)
+https://worldremit.atlassian.net/browse/CONWEB-1745: Pass button aria messages for CLP (#2990)
+https://worldremit.atlassian.net/browse/CONWEB-1519: Regulatory Anti-Fraud Web Form (#2983)
+https://worldremit.atlassian.net/browse/CONWEB-1458: Redirect to country of birth selection page - existing user (#2984)
+*/
