e286d0d751854226c371b6fe0be576c55aeb6aff
Alexey Tomin <alextomin@list.ru>
2022-04-05 12:41:12 +0200

    refactor

--- release-scope.js
+++ release-scope.js
@@ -2,24 +2,24 @@
 
 const fetch = require('isomorphic-fetch')
 const { execSync } = require('child_process');
+const pjson = require('./package.json');
+console.log('script version: ' + pjson.version);
 
 async function getReleaseHash() {
     const releaseHash = await (await fetch('https://www.worldremit.com/public-assets/utils/cms_version.json')).json();
     return releaseHash.commit.substr(0, 9);
 }
 
-function getCommitHashes(branch, fromIndex = 0) {
+function getCommitHashes(branch, fromHash = '') {
     const branchName = branch ? `origin/${branch}` : branch;
-    return execSync(`git log ${branchName} --format='%h'`, { encoding: 'utf-8' })
-        .split('\n')
-        .splice(fromIndex)
+    return execSync(`git log ${branchName} ${fromHash} --format='%h'`, { encoding: 'utf-8' })
+        .split('\n');
 }
 
-function getCommitMessages(branch, fromIndex = 0) {
+function getCommitMessages(branch, fromHash = '') {
     const branchName = branch ? `origin/${branch}` : branch;
-    return execSync(`git log ${branchName} --format='%s'`, { encoding: 'utf-8' })
-        .split('\n')
-        .splice(fromIndex)
+    return execSync(`git log ${branchName} ${fromHash} --format='%s'`, { encoding: 'utf-8' })
+        .split('\n');
 }
 
 function getCurrentBranch() {
@@ -28,25 +28,25 @@ function getCurrentBranch() {
 
 async function main() {
     const [, , releaseBranch = getCurrentBranch(), masterBranch = 'master'] = process.argv;
-    // const res = execSync(`git log ${} --format='%h %s %d '`, { encoding: 'utf-8' });
-    execSync(`git fetch`)
+
+    execSync(`git fetch`);
     const releaseBranchHistory = getCommitHashes(releaseBranch);
     const masterBranchHistory = getCommitHashes(masterBranch);
     const releaseHash = await getReleaseHash();
     // it's possible that release branch has unreleased changes, 
-    // after releaseCommitIndex index everythins is really released
+    // before releaseCommitIndex index everythins is really released
     const releaseCommitIndex = releaseBranchHistory.indexOf(releaseHash);
     if (releaseCommitIndex === -1) {
         throw new Error(`release hash ${releaseHash} wasnt found in release branch logs, first 30:
 ${releaseBranchHistory.slice(0, 30)}`)
     }
-    console.log('current release commit index in release branch', releaseCommitIndex)
-    const releasedCommitMessagesSet = new Set(getCommitMessages(releaseBranch, releaseCommitIndex));
+    console.log('current release commit index in release branch', releaseCommitIndex, 'release commit itself', releaseHash)
+    const releasedCommitMessagesSet = new Set(getCommitMessages(releaseBranch, releaseHash));
     const masterCommitMessages = getCommitMessages(masterBranch)
         .map((message, ind) => ({ message, ind })); // + "ind" field to save original index
     const messagesNotInReleaseBranch = masterCommitMessages.filter(({ message }) => !releasedCommitMessagesSet.has(message))
     console.log('unreleased changes:')
-    console.log(messagesNotInReleaseBranch.map(({message, ind}) => `${masterBranchHistory[ind]} ${message}`).join('\n'))
+    console.log(messagesNotInReleaseBranch.map(({ message, ind }) => `${masterBranchHistory[ind]} ${message}`).join('\n'))
     const jiraNumbers = messagesNotInReleaseBranch
         .filter(x => x.message.includes(':'))
         .map(x => x.message.split(':')[0]);
